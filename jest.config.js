89c723e844dc093842179dbc84e1184f95e59818
Daniel Ostrovsky <daniel.ostrovsky@kaltura.com>
2020-03-31 14:27:12 +0300

    added jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,25 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest'
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html']
+    preset: 'jest-preset-angular',
+    roots: ['apps/art-catalogue/src'],
+    testMatch: ['**/+(*.)+(spec).+(ts)'],
+    setupFilesAfterEnv: ['./apps/art-catalogue/src/test.ts'],
+    collectCoverage: true,
+    coverageReporters: ['html'],
+    reporters: [
+        'default',
+        ['jest-html-reporters', { 'publicPath': 'test-results/html-report' }]
+    ],
+    coverageDirectory: 'test-results/coverage',
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+        prefix: 'apps/art-catalogue/'
+    }),
+    globals: {
+        'ts-jest': {
+            tsConfig: 'apps/art-catalogue/tsconfig.spec.json',
+            diagnostics: false
+        }
+    }
 };
